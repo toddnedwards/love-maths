813ef7738e4251fa43d01cb6f621e9b22e247fa0
toddnedwards <>
2023-09-13 14:01:15 +0000

    display addition qustion to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,28 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
-        });
+        })
     }
+
+    runGame("addition");
 });
 
 /**
  * The main game 'loop', called when the script is first loaded and 
  * aftwer the users answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 
@@ -43,8 +52,10 @@ function incrementWrongAnwer() {
 }
 
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 
